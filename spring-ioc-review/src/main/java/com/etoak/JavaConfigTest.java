44b3d97d21e0a028ad1fec37d4534f40b53ab134
et1911 <et1911@et.com>
2020-05-18 15:50:24 +0800

    提交spring-ioc-review

---
+++ spring-ioc-review/src/main/java/com/etoak/JavaConfigTest.java
@@ -0,0 +1,20 @@
+package com.etoak;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.etoak.action.UserAction;
+import com.etoak.bean.User;
+import com.etoak.config.UserConfig;
+
+public class JavaConfigTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ioc=new AnnotationConfigApplicationContext(UserConfig.class);
+		UserAction userAction = ioc.getBean(UserAction.class);
+		User user = userAction.getById(200);
+		System.out.println(user.getId()+":"+user.getName());
+	}
+
+}
---
+++ spring-ioc-review/src/main/java/com/etoak/config/UserConfig.java
@@ -0,0 +1,26 @@
+package com.etoak.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.etoak.action.UserAction;
+import com.etoak.service.UserService;
+
+@Configuration 
+/* 相当于beans标签 */
+public class UserConfig {
+	@Bean("userService")
+	public UserService userService() {
+		return new UserService();
+	}
+	
+	@Bean
+	public UserAction userAction(@Qualifier("userService") UserService userService) {
+		UserAction userAction = new UserAction();
+		//userAction.setUserService(this.userService());
+		userAction.setUserService(userService);
+		return userAction;
+	}
+
+}
